a59365bebaeaaf40218fe3c315ef285efdbf3363
Sburgin22 <77417578+Sburgin22@users.noreply.github.com>
2022-04-06 17:44:54 -0700

    Update Form1.cs

--- Temperature Conversion/Temperature Conversion/Form1.cs
+++ Temperature Conversion/Temperature Conversion/Form1.cs
@@ -19,14 +19,14 @@ namespace Temperature_Conversion
 
         private void convertToCButton_Click(object sender, EventArgs e)
         {
-            try
+            try //checks to see if input can be changed to double
             {
                 var far = double.Parse(farenheitTextBox.Text);
                 var cel = celsiusTextBox.Text = "";
                 double total = (far - 32) * 5 / 9;
-                outputLabel.Text = (far + " degrees Farenheit is " + total.ToString("n3") + " degrees Celsus");
+                outputLabel.Text = (far + " degrees Farenheit is " + total.ToString("n3") + " degrees Celsus"); //displays temp with 3 decimal places
             }
-            catch (Exception ex)
+            catch (Exception ex) // displays error message if no data/incorrect input detected
             {
                 MessageBox.Show("Please enter a valid value for 'Farenheit'", ex.Message);
             }
@@ -34,14 +34,14 @@ namespace Temperature_Conversion
 
         private void convertToFButton_Click(object sender, EventArgs e)
         {
-            try
+            try //checks to see if input can be changed to double
             {
                 var cel = double.Parse(celsiusTextBox.Text);
                 var far = farenheitTextBox.Text = "";
                 double total = (cel * 9 / 5) + 32;
-                outputLabel.Text = (cel + " degrees Celsius is " + total.ToString("n3") + " degrees Farenheit");
+                outputLabel.Text = (cel + " degrees Celsius is " + total.ToString("n3") + " degrees Farenheit"); //displays temp with 3 decimal places
             }
-            catch
+            catch //error message displayed if no data/incorrect input detected
             {
                 MessageBox.Show("Please enter a valid value for 'Celsius'");
             }
