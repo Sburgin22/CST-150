95f27aca766cc2d84ebb358669f828b4ce85dfcc
Sburgin22 <77417578+Sburgin22@users.noreply.github.com>
2022-04-14 19:07:36 -0700

    Add files via upload

---
+++ Falling Distance/Falling Distance/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Falling_Distance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateDistanceButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.distanceLabel = new System.Windows.Forms.Label();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateDistanceButton
+            // 
+            this.calculateDistanceButton.Location = new System.Drawing.Point(154, 207);
+            this.calculateDistanceButton.Name = "calculateDistanceButton";
+            this.calculateDistanceButton.Size = new System.Drawing.Size(75, 41);
+            this.calculateDistanceButton.TabIndex = 0;
+            this.calculateDistanceButton.Text = "Calculate Distance:";
+            this.calculateDistanceButton.UseVisualStyleBackColor = true;
+            this.calculateDistanceButton.Click += new System.EventHandler(this.calculateDistanceButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(244, 207);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 41);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter falling time in seconds:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Distance Fallen:";
+            // 
+            // distanceLabel
+            // 
+            this.distanceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.distanceLabel.Location = new System.Drawing.Point(219, 138);
+            this.distanceLabel.Name = "distanceLabel";
+            this.distanceLabel.Size = new System.Drawing.Size(100, 23);
+            this.distanceLabel.TabIndex = 4;
+            this.distanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(219, 98);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.timeTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(405, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter time in seconds and I will tell you how far your object has fallen.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 291);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.distanceLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateDistanceButton);
+            this.Name = "Form1";
+            this.Text = "Falling Distance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateDistanceButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label distanceLabel;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Falling Distance/Falling Distance/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Falling_Distance
+{
+    public partial class Form1 : Form
+    {
+        //variables
+        double d; //distance
+        const double g = 9.8; //gravity
+        int t; //time
+
+
+        //calculates the falling distance given t
+        private double FallingDistance(int t)
+        {
+            d = 0.5 * g * Math.Pow(t, 2);//pow gets t to the power of 2
+            return d;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateDistanceButton_Click(object sender, EventArgs e)
+        {
+            //gets user input
+            //if can be converted to int it does so
+            //else returns an error message
+            if(int.TryParse(timeTextBox.Text, out t))
+            {
+                //use fallingDistance to find distance fallen
+                d = FallingDistance(t);
+
+                //display results
+                distanceLabel.Text = d.ToString("n0") + " meters";//n0 displays comma befor thousands place
+            }
+            else
+            {
+                //display error message
+                MessageBox.Show("Please enter an integer value.");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //exits program
+            this.Close();
+        }
+    }
+}
