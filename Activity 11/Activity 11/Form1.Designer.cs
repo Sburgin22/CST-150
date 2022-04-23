8acb365b975cfb85ce33b53a7027462910a661af
Sburgin22 <77417578+Sburgin22@users.noreply.github.com>
2022-04-23 11:02:06 -0700

    Add files via upload

---
+++ Activity 11/Activity 11/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Activity_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rollButton = new System.Windows.Forms.Button();
+            this.die1TextBox = new System.Windows.Forms.TextBox();
+            this.die2TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rollButton
+            // 
+            this.rollButton.Location = new System.Drawing.Point(27, 72);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(75, 23);
+            this.rollButton.TabIndex = 0;
+            this.rollButton.Text = "Roll";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
+            // 
+            // die1TextBox
+            // 
+            this.die1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.die1TextBox.Location = new System.Drawing.Point(154, 37);
+            this.die1TextBox.MaximumSize = new System.Drawing.Size(100, 100);
+            this.die1TextBox.Multiline = true;
+            this.die1TextBox.Name = "die1TextBox";
+            this.die1TextBox.Size = new System.Drawing.Size(100, 100);
+            this.die1TextBox.TabIndex = 1;
+            this.die1TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // die2TextBox
+            // 
+            this.die2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.die2TextBox.Location = new System.Drawing.Point(296, 37);
+            this.die2TextBox.MaximumSize = new System.Drawing.Size(100, 100);
+            this.die2TextBox.Multiline = true;
+            this.die2TextBox.Name = "die2TextBox";
+            this.die2TextBox.Size = new System.Drawing.Size(100, 100);
+            this.die2TextBox.TabIndex = 2;
+            this.die2TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 174);
+            this.Controls.Add(this.die2TextBox);
+            this.Controls.Add(this.die1TextBox);
+            this.Controls.Add(this.rollButton);
+            this.Name = "Form1";
+            this.Text = "Dice Roller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.TextBox die1TextBox;
+        private System.Windows.Forms.TextBox die2TextBox;
+    }
+}
+
---
+++ Activity 11/Activity 11/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Dice
+        {
+           // private field for num of sides
+            private int sides;
+            
+            //constructor that randomly generates sides for the dice
+            public int DieSides()
+            {
+                Random s = new Random();
+                sides = s.Next(4,21);
+                return sides;
+            }
+
+            //roll method
+            public int Roll()
+            {
+                int rollDice;
+                Random r = new Random();
+                rollDice = r.Next(1, sides + 1);
+                return rollDice;
+            }
+        }
+
+        private void rollButton_Click(object sender, EventArgs e)
+        {
+            //counter variable
+            int counter = 0;
+
+            //creates our dice
+            Dice die1 = new Dice();
+            Dice die2 = new Dice();
+
+            //gets dice sides
+            die1.DieSides();
+            die2.DieSides();
+
+            //continuoutsly rolls dice until both die1 and die2 produce ones or snake eyes
+            //adds to counter every time snake eyes not produced
+            do
+            {
+                //displays roll results to text box
+                die1TextBox.Text = die1.Roll().ToString();
+                die2TextBox.Text = die2.Roll().ToString();
+                counter++;
+            }
+            while(die1.Roll() != 1 && die2.Roll() != 1);
+
+            //Message saying how many tries it took to get snake eyes
+            MessageBox.Show("it took " + counter + " tries to get snake eyes.");
+        }
+    }
+}
