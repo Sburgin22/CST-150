50a6d1d14b88c5a8e7a0049d4a8c28997cfa4d76
Sburgin22 <77417578+Sburgin22@users.noreply.github.com>
2022-04-27 18:52:39 -0700

    Add files via upload

---
+++ Activity12/Activity12/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Activity12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordsListBox = new System.Windows.Forms.ListBox();
+            this.findWordsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wordsListBox
+            // 
+            this.wordsListBox.FormattingEnabled = true;
+            this.wordsListBox.Location = new System.Drawing.Point(75, 47);
+            this.wordsListBox.Name = "wordsListBox";
+            this.wordsListBox.Size = new System.Drawing.Size(216, 186);
+            this.wordsListBox.TabIndex = 0;
+            // 
+            // findWordsButton
+            // 
+            this.findWordsButton.Location = new System.Drawing.Point(103, 264);
+            this.findWordsButton.Name = "findWordsButton";
+            this.findWordsButton.Size = new System.Drawing.Size(75, 23);
+            this.findWordsButton.TabIndex = 1;
+            this.findWordsButton.Text = "Find Words";
+            this.findWordsButton.UseVisualStyleBackColor = true;
+            this.findWordsButton.Click += new System.EventHandler(this.findWordsButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(198, 264);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 333);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.findWordsButton);
+            this.Controls.Add(this.wordsListBox);
+            this.Name = "Form1";
+            this.Text = "Activity 12 \"Contains \'t\' or \'e\'\"";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox wordsListBox;
+        private System.Windows.Forms.Button findWordsButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Activity12/Activity12/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Activity12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadFile(List<string> stringList)
+        {
+            try
+            {
+                //open Raven.txt
+                StreamReader inputFile = File.OpenText("Raven.txt");
+
+                //read the words into the list
+                while(!inputFile.EndOfStream)
+                {
+                    stringList.Add(inputFile.ReadLine());
+                }
+
+                //close file
+                inputFile.Close();
+            }
+
+            //return error if file not found
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //displays file contents to listbox
+        private void DisplayLines(List<string> stringList)
+        {
+            foreach(string str in stringList)
+            {
+                wordsListBox.Items.Add(str);
+            }
+        }
+
+        //checks if word ends in 'e', 'E', 't', or 'T'
+        //increases counter if finds word with e or t at end
+        private int EndsIn(List<string> stringList)
+        {
+            //counter variable
+            int wordCount = 0;
+
+            //checks each string in the list
+            //if the last letter is 't' or 'e'
+            //regardless of case, it increases the wordCount
+            foreach(string str in stringList)
+            { 
+                //checks last index of each string
+                //if it ends in e or t increases word count
+                if(str[str.Length - 1] == 'e' || str[str.Length - 1] == 'E' 
+                    || str[str.Length - 1]  == 't' || str[str.Length - 1] == 'T')
+                {
+                    wordCount++;
+                }
+            }
+            return wordCount;
+        }
+
+        private void findWordsButton_Click(object sender, EventArgs e)
+        {
+            int totalWords;
+
+            //creates list to hold strings
+            List<string> stringList = new List<string>();
+
+            //reads words from file to list
+            ReadFile(stringList);
+
+            //displays to textfile
+            wordsListBox.Items.Add("Here are the words from the file:");
+
+            //displays words in list
+            DisplayLines(stringList);
+
+            //get number of words that end in t or e
+            totalWords = EndsIn(stringList);
+
+            //display results
+            wordsListBox.Items.Add(" ");
+            wordsListBox.Items.Add("Total words that end in 't' or 'e': " + totalWords);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closes program
+            this.Close();
+        }
+    }
+}
