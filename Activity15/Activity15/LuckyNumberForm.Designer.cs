676fe0d59348ba1e2f9e69931339416a84cb005c
Sburgin22 <77417578+Sburgin22@users.noreply.github.com>
2022-05-04 18:56:11 -0700

    Add files via upload

---
+++ Activity15/Activity15/LuckyNumberForm.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Activity15
+{
+    partial class LuckyNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LuckyNumberForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MonthComboBox = new System.Windows.Forms.ComboBox();
+            this.DayComboBox = new System.Windows.Forms.ComboBox();
+            this.YearComboBox = new System.Windows.Forms.ComboBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Lucky Number";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Birth Month:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Birth Day:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Birth Year:";
+            // 
+            // MonthComboBox
+            // 
+            this.MonthComboBox.FormattingEnabled = true;
+            this.MonthComboBox.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.MonthComboBox.Location = new System.Drawing.Point(89, 91);
+            this.MonthComboBox.Name = "MonthComboBox";
+            this.MonthComboBox.Size = new System.Drawing.Size(121, 21);
+            this.MonthComboBox.TabIndex = 5;
+            this.MonthComboBox.SelectedIndexChanged += new System.EventHandler(this.MonthComboBox_SelectedIndexChanged);
+            // 
+            // DayComboBox
+            // 
+            this.DayComboBox.FormattingEnabled = true;
+            this.DayComboBox.Location = new System.Drawing.Point(89, 133);
+            this.DayComboBox.Name = "DayComboBox";
+            this.DayComboBox.Size = new System.Drawing.Size(121, 21);
+            this.DayComboBox.TabIndex = 6;
+            // 
+            // YearComboBox
+            // 
+            this.YearComboBox.FormattingEnabled = true;
+            this.YearComboBox.Items.AddRange(new object[] {
+            "2022",
+            "2021",
+            "2020",
+            "2019",
+            "2018",
+            "2017",
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2012",
+            "2011",
+            "2010",
+            "2009",
+            "2008",
+            "2007",
+            "2006",
+            "2005",
+            "2004",
+            "2003",
+            "2002",
+            "2001",
+            "2000",
+            "1999",
+            "1998",
+            "1997",
+            "1996",
+            "1995",
+            "1994",
+            "1993",
+            "1992",
+            "1991",
+            "1990",
+            "1989",
+            "1988",
+            "1987",
+            "1986",
+            "1985",
+            "1984",
+            "1983",
+            "1982",
+            "1981",
+            "1980",
+            "1979",
+            "1978",
+            "1977",
+            "1976",
+            "1975",
+            "1974",
+            "1973",
+            "1972",
+            "1971",
+            "1970",
+            "1969",
+            "1968",
+            "1967",
+            "1966",
+            "1965",
+            "1964",
+            "1963",
+            "1962",
+            "1961",
+            "1960",
+            "1959",
+            "1958",
+            "1957",
+            "1956",
+            "1955",
+            "1954",
+            "1953",
+            "1952",
+            "1951",
+            "1950",
+            "1949",
+            "1948",
+            "1947",
+            "1946",
+            "1945",
+            "1944",
+            "1943",
+            "1942",
+            "1941",
+            "1940",
+            "1939",
+            "1938",
+            "1937",
+            "1936",
+            "1935",
+            "1934",
+            "1933",
+            "1932",
+            "1931",
+            "1930",
+            "1929",
+            "1928",
+            "1927",
+            "1926",
+            "1925",
+            "1924",
+            "1923",
+            "1922",
+            "1921",
+            "1920",
+            "1919",
+            "1918",
+            "1917",
+            "1916",
+            "1915",
+            "1914",
+            "1913",
+            "1912",
+            "1911",
+            "1910",
+            "1909",
+            "1908",
+            "1907",
+            "1906",
+            "1905",
+            "1904",
+            "1903",
+            "1902",
+            "1901",
+            "1900"});
+            this.YearComboBox.Location = new System.Drawing.Point(89, 52);
+            this.YearComboBox.Name = "YearComboBox";
+            this.YearComboBox.Size = new System.Drawing.Size(121, 21);
+            this.YearComboBox.TabIndex = 7;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(89, 195);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 47);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear Form";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(170, 195);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 47);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // LuckyNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 264);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.YearComboBox);
+            this.Controls.Add(this.DayComboBox);
+            this.Controls.Add(this.MonthComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LuckyNumberForm";
+            this.Text = "Lucky Number App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox MonthComboBox;
+        private System.Windows.Forms.ComboBox DayComboBox;
+        private System.Windows.Forms.ComboBox YearComboBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Activity15/Activity15/LuckyNumberForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//program that takes the month, day, and year you were born and generates in a second form your lucky number
+//uses selected indexes from combo boxes to do so
+//the total of the combo box indexes is your lucky number
+
+
+//*ERRORS*
+//user must first select year, then month, then day
+//if year not chosen first error occurs
+
+namespace Activity15
+{
+    public partial class LuckyNumberForm : Form
+    {
+        //variables for our various methods to check day, year and lucky number
+        static int Lucky;
+        private int dNum;
+        private int year;
+
+        ResultsForm r;
+
+        public LuckyNumberForm()
+        {
+            InitializeComponent();
+        }
+
+        //checks if year is leap year or not by seeing if given year
+        //is evenly divisible by 4
+        //if so, it is deemed a leap year and february gains 29 days
+        public bool IsLeapYear(int year)
+        {
+            //flag for leap year
+            bool leapYear = false;
+
+            //turns selected year into integer
+            int selectedYear = int.Parse(YearComboBox.SelectedItem.ToString());
+
+            if(selectedYear % 4 == 0)
+            {
+                leapYear = true;
+            }
+
+            else
+            {
+                leapYear = false;
+            }    
+
+            return leapYear;
+        }
+
+        //designates a specific amount of days based on the index chosen in the
+        //month and year combo boxes. index 1 is February.
+        //as such it then checks if the year is a leap year before displaying 28 or 29 days
+        //most other months, based on their index, such as January, will display 31 days.
+        //every thing else displays with only 30 days
+        public void DAY_NUM()
+        {
+            //dNum designates our day count
+            if(MonthComboBox.SelectedIndex == 1)
+            {
+                if(IsLeapYear(year) == true)
+                {
+                    dNum = 29;
+                }
+                else if(IsLeapYear(year) == false)
+                {
+                    dNum = 28;
+                }
+            }
+
+            //if selected index is one of these numbers
+            //aka january, August, etc there will be 31 days displayed in the day combo box
+            else if(MonthComboBox.SelectedIndex == 0 || MonthComboBox.SelectedIndex == 2 || MonthComboBox.SelectedIndex == 4
+                || MonthComboBox.SelectedIndex == 6 || MonthComboBox.SelectedIndex == 7 || MonthComboBox.SelectedIndex == 9 ||
+                MonthComboBox.SelectedIndex == 11)
+            {
+                dNum = 31;
+            }
+
+            //otherwise display 30 days
+            else
+            {
+                dNum = 30;
+            }
+
+            //so long as month combo box has a selected value
+            //display the number of days based on month
+            if(MonthComboBox.SelectedIndex != -1)
+            {
+                DayComboBox.Items.Clear();
+                for(int day = 0; day < dNum; day++)
+                {
+                    DayComboBox.Items.Add(day + 1);
+                }
+            }
+
+            //displays the days to the day combo box
+            DayComboBox.Text = dNum.ToString();
+        }
+
+        //returns our lucky number
+        public int LuckyNum()
+        {
+            return Lucky;
+        }
+
+        //finds lucky number and opens up the second form to display the result
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(YearComboBox.SelectedIndex != -1 && DayComboBox.SelectedIndex != -1 && MonthComboBox.SelectedIndex != -1)
+            {
+                Lucky = (MonthComboBox.SelectedIndex + DayComboBox.SelectedIndex + YearComboBox.SelectedIndex);
+                r = new ResultsForm();
+                r.ShowDialog();
+            }
+
+            else
+            {
+                MessageBox.Show("Please fill in the month, day, and year boxes.");
+            }
+        }
+
+        //changes number of days in month based off of year and month
+        private void MonthComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DAY_NUM();
+        }
+
+        private void YearComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            DAY_NUM();
+        }
+
+        //resets the combo box values
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            YearComboBox.Text = "";
+            DayComboBox.Text = "";
+            MonthComboBox.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closes program
+            this.Close();
+        }
+    }
+}
